1cd8ed795be770da9d05c968a29d480a7b312894
Abner Santos <abner.santos@usp.br>
2020-10-20 15:00:46 -0300

    Changed ifs to a switch

--- path_to_img.py
+++ path_to_img.py
@@ -2,7 +2,7 @@ import sys
 import imageio
 import numpy as np
 
-def load_maze(filename):
+def file_load(filename):
     
     try:
         with open(filename, 'r') as text_file:
@@ -25,44 +25,30 @@ def load_maze(filename):
 
     return maze
 
-def str_to_img(maze):
+def img_converter(maze):
 
     img = np.zeros((len(maze), len(maze[0]), 3))
 
+    color = {
+        '*': [255, 214, 191],
+        '-': [48, 18, 45],
+        'x': [135, 7, 52],
+        'o': [203, 45, 62],
+        '#': [77, 8, 42],
+        '$': [239, 71, 58]
+    }
+
     for x in range(0, img.shape[0]):
         for y in range(0, img.shape[1]):
-        
-            # Empty space
-            if maze[x][y] == '*':
-                img[x][y] = [255, 214, 191]
-
-            # Obstacle
-            elif maze[x][y] == '-':
-                img[x][y] = [48, 18, 45]
-
-            # Analyzed paths
-            elif maze[x][y] == 'x':
-                img[x][y] = [135, 7, 52]
-
-            # Found path
-            elif maze[x][y] == 'o':
-                img[x][y] = [203, 45, 62]
-
-            # Spawn
-            elif maze[x][y] == '#':
-                img[x][y] = [77, 8, 42]
-
-            # End
-            elif maze[x][y] == '$':
-                img[x][y] = [239, 71, 58]
+            img[x][y] = color.get(maze[x][y])
 
     return img
 
 # Loads string
-maze = load_maze(sys.argv[1])
+maze = file_load(sys.argv[1])
 
 # Transforms it into an image
-img = str_to_img(maze)
+img = img_converter(maze)
 
 # Writes image
 imageio.imwrite(sys.argv[2], img.astype(np.uint8))
